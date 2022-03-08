c0c362ea62b6051db6c4f0d29236168242b2e0d9
ChuchuMmm <98064121+ChuchuMmm@users.noreply.github.com>
2022-03-07 18:20:41 -0600

    added department, nurse, med staff

--- medicalClinicSQL.sql
+++ medicalClinicSQL.sql
@@ -34,5 +34,52 @@ CREATE TABLE patient(
     zipcode VARCHAR(10)
 );
 
+DROP TABLE department;
+CREATE TABLE department{
+    department_id VARCHAR(10) PRIMARY KEY UNIQUE,
+    department_name VARCHAR(30) NOT NULL,
+    employee_id CHAR(10) NOT NULL,
+    staff_count INT,
+    office_id CHAR(10) NOT NULL,
+    PRIMARY KEY(employee_id),
+    FOREIGN KEY(employee_id) REFERENCES clinic_employee(employee_id),
+    PRIMARY KEY(office_id,)
+    FOREIGN KEY(office_id) REFERENCES office(office_id),
+};
+
+DROP TABLE nurse;
+CREATE TABLE nurse{
+    employee_id CHAR(10) NOT NULL,
+    f_name VARCHAR(20) NOT NULL,
+    l_name VARCHAR(20) NOT NULL,
+    birth_date DATE,
+    sex VARCHAR(10),
+    phone_number VARCHAR(15),
+    position VARCHAR(20),
+    hired DATETIME NOT NULL,
+    department_id VARCHAR(10),
+    PRIMARY KEY(employee_id),
+    FOREIGN KEY(employee_id) REFERENCES clinic_employee(employee_id),
+    PRIMARY KEY(office_id,)
+    FOREIGN KEY office_id) REFERENCES office(office_id),
+}
+
+DROP TABLE med_staff;
+CREATE TABLE med_staff{
+    employee_id CHAR(10) NOT NULL,
+    f_name VARCHAR(20) NOT NULL,
+    l_name VARCHAR(20) NOT NULL,
+    birth_date DATE,
+    sex VARCHAR(10),
+    phone_number VARCHAR(15),
+    position VARCHAR(20),
+    hired DATETIME NOT NULL,
+    department_id VARCHAR(10),
+    PRIMARY KEY(employee_id),
+    FOREIGN KEY(employee_id) REFERENCES clinic_employee(employee_id),
+    PRIMARY KEY(office_id,)
+    FOREIGN KEY office_id) REFERENCES office(office_id),
+}
+
 INSERT INTO patient VALUE ("Malik", "Taylor");
 SELECT * FROM patient
