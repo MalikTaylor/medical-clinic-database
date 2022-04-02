886ec505d4b65562d9bf101819729f12875a6aad
MalikTaylor <67035475+MalikTaylor@users.noreply.github.com>
2022-04-02 17:25:44 -0500

    added error response to mysqli_connect.php file

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -10,7 +10,7 @@
     // OR die("Could not connect to MySQL " . mysqli_connect_error());
 
     //Get Heroku ClearDB connection information
-    
+
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $cleardb_server = $cleardb_url["host"];
     $cleardb_username = $cleardb_url["user"];
@@ -19,5 +19,6 @@
     $active_group = 'default';
     $query_builder = TRUE;
     // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db)
+    OR die("Could not connect to MySQL " . mysqli_connect_error());
 ?>
